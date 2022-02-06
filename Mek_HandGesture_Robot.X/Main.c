cad3e744fb19b92d13be41ba12d92572daabfcb4
Sjensinn <66678259+Sjensinn@users.noreply.github.com>
2022-02-06 22:07:54 +0000

    Update Main.c

--- Mek_HandGesture_Robot.X/Main.c
+++ Mek_HandGesture_Robot.X/Main.c
@@ -22,14 +22,23 @@ void putch(uint8_t txData);
 
 void main(void) {
     system_init();                  //Initiate clock, pins, uart, i2c, timer1 and interrupts
-    //PCA_Init(130, 0x80);          //Initiate PCA9685 module with 130 prescalar and address = 0x80
+    PCA_Init(130, 0x80);          //Initiate PCA9685 module with 130 prescalar and address = 0x80
     //LCD_init(0x4E);               // Initialize LCD module with I2C address = 0x4E
     //sensor_init();                //Initialize the TCS3200 sensor
     
-    while(1){                       //Embedded systems never stop having fun!
-        //I like to git it git it
-        LED_TOGGLE();               //And we have rave lights
-        __delay_ms(1000); 
+    INTCONbits.GIE = 1;         //Enable Global interrupts
+    INTCONbits.PEIE = 1;        //Enable Pheripheral Interrupts
+
+    PCA_Init(130 ,0x80);     //Initiate PCA driver with the address(0x80) and prescalar (decimal 130)
+
+    while(1){
+        LATBbits.LATB2 = 0;             //Min range
+        PCA_write(0, 4000, 95);         //4095 is max resolution, On for 4000/4095, off for 95/4095
+        delay_ms(5000);
+
+        LATBbits.LATB2 = 1;             //Max range
+        PCA_write(0, 3595, 500);        //4095 is max resolution, On for 500/4095, Off for 3595/4095
+        delay_ms(5000);
     }
     return;
 }
